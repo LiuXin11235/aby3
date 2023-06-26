dc06592debb361a68c67561c904b36a889710e8b
Fannxy <fanxy20@mails.tsinghua.edu.cn>
2023-06-26 15:56:58 +0000

    update the real metric function test case

--- aby3-RTR/PTRTest.h
+++ aby3-RTR/PTRTest.h
@@ -35,6 +35,9 @@ int test_cipher_mean_distance(oc::CLP& cmd, size_t n, size_t m, size_t k, int ta
 int test_cipher_bio_metric(oc::CLP& cmd, size_t n, size_t m, size_t k, int task_num,
                               int opt_B);
 
+int test_cipher_metric(oc::CLP& cmd, size_t n, size_t m, size_t k, int task_num,
+                              int opt_B);
+
 int profile_index(oc::CLP& cmd, size_t n, size_t m, int vector_size, int task_num);
 
 int probe_profile_index(oc::CLP& cmd, size_t n, size_t m, int vector_size_start,
