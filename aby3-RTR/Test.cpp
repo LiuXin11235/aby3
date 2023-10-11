d3170e31f2989f744a7a1c5eb9d79343ee6a5261
Fannxy <fanxy20@mails.tsinghua.edu.cn>
2023-10-11 02:14:09 +0000

    update test

---
+++ aby3-RTR/Test.cpp
@@ -0,0 +1,12 @@
+#include "Test.h"
+#include <chrono>
+#include <thread>
+#include "BuildingBlocks.h"
+
+using namespace oc;
+using namespace aby3;
+using namespace std;
+
+int basic_test(CLP& cmd){
+    
+}
---
+++ aby3-RTR/Test.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <cryptoTools/Common/CLP.h>
+#include <cryptoTools/Network/IOService.h>
+
+int basic_test(oc::CLP& cmd);
