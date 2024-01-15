9023b18007334f7d9cf2b23a31b27fe2d5cbc4bf
Fannxy <fxy23@mails.tsinghua.edu.cn>
2024-01-15 06:32:46 +0000

    update main

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -7,29 +7,21 @@
 #include "aby3_tests/aby3_tests.h"
 #include "eric.h"
 
-
 using namespace oc;
 using namespace aby3;
-// std::vector<std::string> unitTestTag{ "u", "unitTest" };
 
 int main(int argc, char** argv) {
   oc::CLP cmd(argc, argv);
   // reinit the environment and then finalize the environment.
-//   MPI_Init(&argc, &argv);
 
-  // if (cmd.isSet(unitTestTag)){
-  // 	auto tests = aby3_tests;
-  //   tests.runIf(cmd);
-  //   return 0;
-  // }
+  // set the role for this process.
+	if (cmd.isSet("Bool")){
+		bool_basic_test(cmd);
+	}
 
-  if (cmd.isSet("Bool")){
-    bool_basic_test(cmd);
-  }
-
-  if (cmd.isSet("Arith")){
-    arith_basic_test(cmd);
-  }
+	if (cmd.isSet("Arith")){
+		arith_basic_test(cmd);
+	}
 
   return 0;
 }
